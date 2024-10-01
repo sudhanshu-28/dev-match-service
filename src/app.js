42be74ae6cabdae5b561270f24cf133e96195a16
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-01 21:53:57 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+// This callback function passed inside use function is called Request Handler
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
+});
+
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello hello!");
+});
+
+app.use("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.listen(7777, () => {
+  console.log("Server is running on port 7777...");
+});
