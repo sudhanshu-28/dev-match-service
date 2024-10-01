e61dad14e32c1c21f96194b96ca1402ba34d1bd8
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-02 01:23:24 +0530

    Exploring Routes and HTTP methods using Express.

--- src/app.js
+++ src/app.js
@@ -2,18 +2,36 @@ const express = require("express");
 
 const app = express();
 
-// This callback function passed inside use function is called Request Handler
+// get : This will only handle GET route call to /user
+app.get("/user", (req, res) => {
+  res.send("Users List API trigerred");
+});
+
+app.post("/user", (req, res) => {
+  res.send("User Create API trigerred");
+});
+
+app.put("/user", (req, res) => {
+  res.send("User Update Profile API trigerred");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("User Update API trigerred");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User Delete API trigerred");
+});
+
+// use: This will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
-  res.send("Hello from the server");
+  res.send("Hello from Test API call");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("Hello hello hello!");
-});
-
-app.use("/", (req, res) => {
-  res.send("Home Page");
-});
+// This callback function passed inside use function is called Request Handler
+// app.use("/", (req, res) => {
+//   res.send("Main Dashboard API call");
+// });
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777...");
