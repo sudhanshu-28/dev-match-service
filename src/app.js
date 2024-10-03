fc08ae1c35a8b33a919db03f699684078b8631f9
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-03 21:53:24 +0530

    Multiple Route Handlers.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,30 @@ const express = require("express");
 
 const app = express();
 
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Route Handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route Handler 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route Handler 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route Handler 4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Route Handler 5");
+    next();
+  }
+);
+
 app.listen(7777, () => {
   console.log("Server is running on port 7777...");
 });
