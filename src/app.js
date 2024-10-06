0ab7dd4dc36c4620375f33000c87bfbe9f3bd207
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-06 21:12:34 +0530

    User APIs added.

--- src/app.js
+++ src/app.js
@@ -9,17 +9,138 @@ const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
-
   // Creating new instance of User model
-  const user = new User(req.body);
-  // // Recommend way - with All DB operations best practice is to write in try catch
+  const user = new User(req?.body);
+
+  // Recommend way - with All DB operations best practice is to write in try catch
   try {
     await user.save();
     res.send("User added successfully!");
   } catch (error) {
     console.log("Error => ", err);
     res.status(500).send("User failed to add.");
+  }
+});
+
+app.get("/getAllUsers", async (req, res) => {
+  try {
+    const users = await User.find({});
+    const count = await User.countDocuments();
+
+    const usersList = users.map((user) => ({
+      _id: user?._id,
+      firstName: user?.firstName,
+      lastName: user?.lastName,
+      emailId: user?.emailId,
+      age: user?.age,
+      gender: user?.gender,
+    }));
+
+    if (usersList.length === 0) {
+      res.status(404).send({
+        success: false,
+        message: "Users not found.",
+      });
+    }
+
+    res.send({
+      success: true,
+      message: "Users list fetched successfully.",
+      data: usersList,
+      totalUsers: count,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: "Something went wrong. Failed to fetch Users list.",
+    });
+  }
+});
+
+app.get("/getUserById", async (req, res) => {
+  const userId = req?.query?.id;
+
+  const userData = await User.findById(userId).exec();
+
+  if (!userData) {
+    res.status(404).send({
+      status: false,
+      message: "User not found.",
+    });
+  } else {
+    res.send({
+      success: true,
+      message: "User fetched successfully.",
+      data: {
+        _id: userData?._id,
+        firstName: userData?.firstName,
+        lastName: userData?.lastName,
+        emailId: userData?.emailId,
+        age: userData?.age,
+        gender: userData?.gender,
+      },
+    });
+  }
+});
+
+app.get("/getUserByEmail", async (req, res) => {
+  const userEmailID = req?.query?.emailID;
+
+  const userData = await User.findOne({ emailId: userEmailID });
+
+  if (!userData) {
+    res.status(404).send({
+      success: false,
+      message: "User not found.",
+    });
+  } else {
+    res.send({
+      success: true,
+      message: "User fetched successfully.",
+      data: {
+        _id: userData?._id,
+        firstName: userData?.firstName,
+        lastName: userData?.lastName,
+        emailId: userData?.emailId,
+        age: userData?.age,
+        gender: userData?.gender,
+      },
+    });
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    const count = await User.countDocuments();
+
+    const usersList = users.map((user) => ({
+      _id: user?._id,
+      firstName: user?.firstName,
+      lastName: user?.lastName,
+      emailId: user?.emailId,
+      age: user?.age,
+      gender: user?.gender,
+    }));
+
+    if (usersList.length === 0) {
+      res.status(404).send({
+        success: false,
+        message: "Users not found.",
+      });
+    }
+
+    res.send({
+      success: true,
+      message: "Users list fetched successfully.",
+      data: usersList,
+      totalUsers: count,
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: "Something went wrong. Failed to fetch Users list.",
+    });
   }
 });
 
