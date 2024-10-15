3a1f66bd6d355b1d802e3c022211a40febf1be36
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-15 14:08:00 +0530

    Connection Request API created for interested.

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,16 +1,47 @@
 const express = require("express");
 
+const User = require("../models/user");
+const ConnectionRequest = require("../models/connectionRequest");
+
 const { userAuth } = require("../middlewares/auth");
+const { default: mongoose } = require("mongoose");
 
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req, res) => {
+requestRouter.post("/send/interested/:userId", userAuth, async (req, res) => {
   try {
-    const { firstName } = req?.user;
+    const { _id: fromUserId } = req?.user;
+    const { userId: toUserId } = req?.params;
+
+    // Validate request ID
+    if (!mongoose.isValidObjectId(toUserId)) {
+      throw new Error(
+        "Invalid User Id. Could not able to sent connection request."
+      );
+    }
+
+    // Check user exist with this ID or not
+    const user = await User.findById(toUserId);
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+
+    // Store Connection request
+    const newConnectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status: "interested",
+    });
+
+    await newConnectionRequest.save();
 
     res.send({
       success: true,
-      message: firstName + " has sent Connection Request.",
+      message:
+        "Connection Request has been sent to " +
+        user?.firstName +
+        " successfully.",
     });
   } catch (error) {
     res.status(400).send({
