7f044329cb92e833140660a53cfcc4712d1df633
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-10-22 23:45:52 +0530

    Socket.IO In progress changes.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const http = require("node:http");
+const path = require("node:path");
+const { Server } = require("socket.io");
+
 const connectDB = require("./config/database");
 
 const {
@@ -14,6 +18,7 @@ const {
 
 const app = express();
 
+// CORS configuration
 app.use(
   cors({
     origin: "http://localhost:5173", // White listing this domain name
@@ -27,17 +32,44 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
+// Create an HTTP server from the Express app
+const server = http.createServer(app);
+
+// Create a Socket.IO server instance
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Match your React app's origin
+    methods: ["GET", "POST"], // Allowed methods for WebSocket
+    credentials: true, // Pass credentials if needed
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("client-message", (message) => {
+    io.emit("server-message", message);
+  });
+});
+
 app.use("/auth", authRouter);
 app.use("/profile", profileRouter);
 app.use("/request", requestRouter);
 app.use("/user", userRouter);
 app.use("/example", exampleRouter);
 
+app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
 // Start your sever only when you are connected to Database
 connectDB()
   .then(() => {
     console.log("Database connection established...");
-    app.listen(7777, () => {
+
+    // Start the server on port 7777 (both HTTP API and WebSocket)
+    server.listen(7777, () => {
       console.log("Server is running on Port:7777...");
     });
   })
